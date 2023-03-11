682f9abbe44c26795f7ebbb32edd05a70813fe02
Denis <denislee98@gmail.com>
2023-03-11 23:46:43 +0100

    added pre commit

--- src/settings.py
+++ src/settings.py
@@ -6,13 +6,16 @@ load_dotenv()
 
 
 # Database
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
 
 # Redis
-REDIS_HOST = os.environ.get('REDIS_HOST')
-REDIS_PORT = os.environ.get('REDIS_PORT')
-REDIS_DB = os.environ.get('REDIS_DB')
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = os.environ.get("REDIS_PORT")
+REDIS_DB = os.environ.get("REDIS_DB")
+
+# API
+HH_URL = os.environ.get("HH_URL")
--- src/vacancies/tasks.py
+++ src/vacancies/tasks.py
@@ -1,5 +1,8 @@
 import logging
+import requests
+from requests.exceptions import HTTPError
 
+from src.settings import HH_URL
 from src.celery_app.app import app
 
 
@@ -8,4 +11,13 @@ _logger = logging.getLogger(__name__)
 
 @app.task
 def collect_vacancies_from_hh():
-    _logger.error('celery test')
+    try:
+        requests.get(HH_URL)
+    except HTTPError as e:
+        _logger.error(
+            f"HTTP error occurred while collect vacancies from hh: {e}"
+        )
+    except Exception as err:
+        _logger.error(f"Error occurred while collect vacancies from hh: {err}")
+
+    _logger.error("celery test")
