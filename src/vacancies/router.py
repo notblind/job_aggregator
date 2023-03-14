3bcb02d269d8ebebad7b4b6b9f1af5b7db743b86
Denis <denislee98@gmail.com>
2023-03-15 00:16:04 +0100

    init api vacancies

--- src/vacancies/router.py
+++ src/vacancies/router.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_async_session
+from src.vacancies.models import Vacancy
 
 router = APIRouter(
     prefix='/vacancies/v1',
@@ -7,5 +12,7 @@ router = APIRouter(
 
 
 @router.get('/vacancies')
-async def api_vacancies():
-    return
+async def api_vacancies(session: AsyncSession = Depends(get_async_session)):
+    query = select(Vacancy)
+    result = await session.execute(query)
+    return result.all()
